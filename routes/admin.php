745d110f02192cb3ecae31a5b4a0f3634b4e06ad
ngocbeo123 <nguyendinhngoc12301@gmail.com>
2025-10-07 09:44:33 +0700

    Add local moment.js vendor files

--- routes/admin.php
+++ routes/admin.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', function () {
         return view('admin.pages.dashboard');
-    });
+    })->name('admin.dashboard');
+
+    // Hiển thị form đăng nhập
     Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+
+    // Xử lý khi submit form đăng nhập
+    Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login.post');
 });
+
