796de018685baea85d1f01430e35a835d4f1b33e
hungdv36 <hungdvph48701@gmail.com>
2025-10-08 15:29:49 +0700

    Danh sách danh mục, hiển thị sản phẩm và lọc sản phẩm(danh mục, giá)

--- public/assets/clients/js/main.js
+++ public/assets/clients/js/main.js
@@ -1243,18 +1243,78 @@
         /* ---------------------------------------------------------
             32. Price Slider
         --------------------------------------------------------- */
-        $( ".slider-range" ).slider({
-            range: true,
-            min: 50,
-            max: 5000,
-            values: [ 50, 1500 ],
-            slide: function( event, ui ) {
-                $( ".amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-            }
-        });
-        $( ".amount" ).val( "$" + $( ".slider-range" ).slider( "values", 0 ) +
-        " - $" + $( ".slider-range" ).slider( "values", 1 ) ); 
+        // $( ".slider-range" ).slider({
+        //     range: true,
+        //     min: 50,
+        //     max: 5000,
+        //     values: [ 50, 1500 ],
+        //     slide: function( event, ui ) {
+        //         $( ".amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        //     }
+        // });
+        // $( ".amount" ).val( "$" + $( ".slider-range" ).slider( "values", 0 ) +
+        // " - $" + $( ".slider-range" ).slider( "values", 1 ) ); 
+function fetchProducts() {
+    let category_id = $(".category-filter.active").data("id") || '';
+    let minPrice = $(".slider-range").slider("values", 0);
+    let maxPrice = $(".slider-range").slider("values", 1);
+    let sort_by = $("#sort-by").val();
 
+    $.ajax({
+        url: '/products/filter',
+        type: 'GET',
+        data: {
+            category_id: category_id,
+            min_price: minPrice,
+            max_price: maxPrice,
+            sort_by: sort_by
+        },
+        beforeSend: function() {
+            $("#loading-spinner").show();
+            $("#liton_product_grid").hide();
+        },
+        success: function(response) {
+            $("#liton_product_grid").html(response.products);
+        },
+        
+        complete: function() {
+            $("#loading-spinner").hide();
+            $("#liton_product_grid").show();
+        },
+        error: function(xhr) {
+           alert('Có lỗi xảy ra với ajax fetchProducts');
+        }
+    });
+}
+
+
+$(".category-filter").on("click", function () {
+    $(".category-filter").removeClass("active");
+    $(this).addClass("active");
+    fetchProducts();
+});
+$("#sort-by").change(function () {
+    fetchProducts();
+});
+
+$(".slider-range").slider({
+    range: true,
+    min: 0,
+    max: 300000,
+    values: [0, 300000],
+    slide: function(event, ui) {
+        $(".amount").val(ui.values[0] + " - " + ui.values[1] + " VNĐ");
+    },
+    change: function(event, ui) {
+        fetchProducts();
+    }
+});
+
+$(".amount").val(
+    $(".slider-range").slider("values", 0) +
+    " - " +
+    $(".slider-range").slider("values", 1) + " VNĐ"
+);
 
         /* --------------------------------------------------------
             33. Quantity plus minus
