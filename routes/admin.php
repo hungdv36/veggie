af3c717bfec4ba7fb53ad761410da5ca896e6ece
AQuan05 <trandihanhquan17072021@gmail.com>
2025-10-09 17:31:46 +0700

    login admin (done)

--- routes/admin.php
+++ routes/admin.php
@@ -3,23 +3,29 @@
 use App\Http\Controllers\Admin\AdminAuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\Admin\CategoryController;
 
-Route::prefix('admin')->group(function () {
-    // Trang dashboard
+Route::prefix('admin')->name('admin.')->group(function () {
+    // Dashboard
     Route::get('/dashboard', function () {
         return view('admin.pages.dashboard');
-    })->name('admin.dashboard');
+    })->name('dashboard');
 
     // Login
     Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AdminAuthController::class, 'login']);
+    Route::post('/login', [AdminAuthController::class, 'login'])->name('login.post');
+    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('logout'); // sửa ở đây
 
     // Users
     Route::middleware(['permission:manage_users'])->group(function () {
-        Route::get('/users', [UsersController::class, 'index'])->name('admin.users.index');
-        Route::post('/user/upgrade', [UsersController::class, 'upgrade'])->name('admin.user.upgrade');
-        Route::post('/user/updateStatus', [UsersController::class, 'updateStatus'])->name('admin.user.updateStatus');
-        Route::post('/user/toggleDelete', [UsersController::class, 'toggleDelete'])
-            ->name('admin.user.toggleDelete');
+        Route::get('/users', [UsersController::class, 'index'])->name('users.index');
+        Route::post('/user/upgrade', [UsersController::class, 'upgrade'])->name('user.upgrade');
+        Route::post('/user/updateStatus', [UsersController::class, 'updateStatus'])->name('user.updateStatus');
+        Route::post('/user/toggleDelete', [UsersController::class, 'toggleDelete'])->name('user.toggleDelete');
+    });
+
+    // Categories
+    Route::middleware(['auth', 'check.admin'])->group(function () {
+        Route::resource('categories', CategoryController::class);
     });
 });
