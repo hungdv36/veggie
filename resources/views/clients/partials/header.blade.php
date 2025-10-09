7ee31912569292026593a4ede11e4442ae8465fe
AQuan05 <trandihanhquan17072021@gmail.com>
2025-10-09 23:23:31 +0700

    fix addtocart

--- resources/views/clients/partials/header.blade.php
+++ resources/views/clients/partials/header.blade.php
@@ -133,7 +133,6 @@
                     <div class="mini-cart-icon">
                         <a href="#ltn__utilize-cart-menu" class="ltn__utilize-toggle">
                             <i class="icon-shopping-cart"></i>
-                            <sup>2</sup>
                         </a>
                     </div>
                     <!-- mini-cart -->
