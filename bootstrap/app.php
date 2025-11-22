c7b6a7527c1ecf73f884452bf69f14debc3a844e
slkfmgkj <duongldph33024@fpt.edu.vn>
2025-11-22 16:49:15 +0700

    check các thiệt bị đã truy cập website

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+         $middleware->web([
+        App\Http\Middleware\TrackDevice::class,
+    ]);
         $middleware->alias([
+            'track.device' => App\Http\Middleware\TrackDevice::class,
             'auth.custom' => App\Http\Middleware\RedirectIfNotAuthenticated::class,
             'permission' => App\Http\Middleware\CheckPermission::class,
         ]);
